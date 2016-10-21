59250570e98249c73d838da537554109cce10e07
ZongGenXu <540682674@qq.com>
2016-10-21 16:19:11 +0800

    修改pages

---
+++ Project/Calendar/ZFCalendar-master/README.md
@@ -0,0 +1,20 @@
+#类似去哪儿中的日历控件,显示农历，农历节日，公历节日,公历日期
+
+Github：
+
+（1）导入头文件
+
+import "CalendarHomeViewController.h"<br>
+import "CalendarViewController.h"<br>
+import "Color.h"
+
+（2）声明CalendarHomeViewController实例变量：
+
+CalendarHomeViewController *chvc;
+
+（3）初始化方法：
+
+[chvc setAirPlaneToDay:365 ToDateforString:nil];
+
+![](https://github.com/zhangfan520/ZFCalendar/raw/master/IMG_0244.jpg)
+![](https://github.com/zhangfan520/ZFCalendar/raw/master/IMG_0245.jpg)
---
+++ Project/Calendar/ZFCalendar-master/ZFCalendar/ZFCalendar/ViewController.m
@@ -0,0 +1,100 @@
+//
+//  ViewController.m
+//  ZFCalendar
+//
+//  Created by 张凡 on 15/12/3.
+//  Copyright © 2015年 张凡. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#import "CalendarHomeViewController.h"
+#import "CalendarViewController.h"
+#import "Color.h"
+
+@interface ViewController (){
+    
+    CalendarHomeViewController *chvc;
+    
+}
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    CGFloat width = self.view.bounds.size.width;
+    
+    UIButton *but1 = [[UIButton alloc]initWithFrame:CGRectMake(10, 100, width-20, 50)];
+    but1.backgroundColor = COLOR_THEME;
+    but1.tintColor = [UIColor whiteColor];
+    but1.tag = 1;
+    [but1 setTitle:@"飞机票" forState:UIControlStateNormal];
+    [but1 addTarget:self action:@selector(click:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:but1];
+    
+    
+    UIButton *but2 = [[UIButton alloc]initWithFrame:CGRectMake(10, 200, width-20, 50)];
+    but2.backgroundColor = COLOR_THEME;
+    but2.titleLabel.textColor = [UIColor whiteColor];
+    but2.tag = 2;
+    [but2 setTitle:@"火车票" forState:UIControlStateNormal];
+    [but2 addTarget:self action:@selector(click:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view  addSubview:but2];
+    
+    
+    UIButton *but3 = [[UIButton alloc]initWithFrame:CGRectMake(10, 300, width-20, 50)];
+    but3.backgroundColor = COLOR_THEME;
+    but3.titleLabel.textColor = [UIColor whiteColor];
+    but3.tag = 3;
+    [but3 setTitle:@"酒店" forState:UIControlStateNormal];
+    [but3 addTarget:self action:@selector(click:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:but3];
+}
+
+
+-(void)click:(UIButton *)but
+{
+    
+    if (!chvc) {
+        
+        chvc = [[CalendarHomeViewController alloc]init];
+        
+        chvc.calendartitle = @"飞机";
+        
+        [chvc setAirPlaneToDay:365 ToDateforString:nil];//飞机初始化方法
+        
+    }
+    
+    
+    
+    
+    
+    chvc.calendarblock = ^(CalendarDayModel *model){
+        
+        NSLog(@"\n---------------------------");
+        NSLog(@"1星期 %@",[model getWeek]);
+        NSLog(@"2字符串 %@",[model toString]);
+        NSLog(@"3节日  %@",model.holiday);
+        
+        if (model.holiday) {
+            
+            [but setTitle:[NSString stringWithFormat:@"%@ %@ %@",[model toString],[model getWeek],model.holiday] forState:UIControlStateNormal];
+            
+        }else{
+            
+            [but setTitle:[NSString stringWithFormat:@"%@ %@",[model toString],[model getWeek]] forState:UIControlStateNormal];
+            
+        }
+    };
+    
+    [self.navigationController pushViewController:chvc animated:YES];
+    
+    
+    
+}
+
+
+@end
